49a41fb3a99b79b976cbc0fbe52e0f2cd43e5bb7
Robert David <robert.david@realeyesit.com>
2014-09-24 17:37:11 +0200

    Add initial PENCIL stdlib
    Functions used in CARP-Benchmarks

--- etc/pencil_headers/pencil_prototypes.h
+++ etc/pencil_headers/pencil_prototypes.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2014, ARM Limited
+ * Copyright (c) 2014, Realeyes
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +24,72 @@
 #ifndef PENCIL_PROTOTYPES_H
 #define PENCIL_PROTOTYPES_H
 
-int max(int x, int y);
-int min(int x, int y);
+#define __PENCIL_IMPL_DECLARE_OVERLOADS(name, param) \
+    __PENCIL_IMPL_DECLARE_OVERLOADS_ ## param(name);
+
+#define __PENCIL_IMPL_DECLARE_OVERLOADS_FLT(functionname)    \
+    float  __attribute((overloadable)) functionname(float ); \
+    double __attribute((overloadable)) functionname(double); \
+
+#define __PENCIL_IMPL_DECLARE_OVERLOADS_FLT_FLT(functionname)        \
+    float  __attribute((overloadable)) functionname(float , float ); \
+    double __attribute((overloadable)) functionname(double, double); \
+
+#define __PENCIL_IMPL_DECLARE_OVERLOADS_FLT_FLTPTR(functionname)      \
+    float  __attribute((overloadable)) functionname(float , float *); \
+    double __attribute((overloadable)) functionname(double, double*); \
+
+#define __PENCIL_IMPL_DECLARE_OVERLOADS_INT_INT(functionname)                                            \
+      signed short int __attribute((overloadable)) functionname(  signed short int,   signed short int); \
+      signed       int __attribute((overloadable)) functionname(  signed       int,   signed       int); \
+      signed  long int __attribute((overloadable)) functionname(  signed  long int,   signed  long int); \
+    unsigned short int __attribute((overloadable)) functionname(unsigned short int, unsigned short int); \
+    unsigned       int __attribute((overloadable)) functionname(unsigned       int, unsigned       int); \
+    unsigned  long int __attribute((overloadable)) functionname(unsigned  long int, unsigned  long int); \
+                  char __attribute((overloadable)) functionname(              char,               char); \
+      signed      char __attribute((overloadable)) functionname(  signed      char,   signed      char); \
+    unsigned      char __attribute((overloadable)) functionname(unsigned      char, unsigned      char); \
+
+#define __PENCIL_IMPL_DECLARE_OVERLOADS_FLT_FLT_FLT(functionname)            \
+    float  __attribute((overloadable)) functionname(float , float , float ); \
+    double __attribute((overloadable)) functionname(double, double, double); \
+
+#define __PENCIL_IMPL_DECLARE_OVERLOADS_INT_INT_INT(functionname)                                                          \
+    signed short int __attribute((overloadable)) functionname(  signed short int,   signed short int,   signed short int); \
+    signed       int __attribute((overloadable)) functionname(  signed       int,   signed       int,   signed       int); \
+    signed  long int __attribute((overloadable)) functionname(  signed  long int,   signed  long int,   signed  long int); \
+  unsigned short int __attribute((overloadable)) functionname(unsigned short int, unsigned short int, unsigned short int); \
+  unsigned       int __attribute((overloadable)) functionname(unsigned       int, unsigned       int, unsigned       int); \
+  unsigned  long int __attribute((overloadable)) functionname(unsigned  long int, unsigned  long int, unsigned  long int); \
+                char __attribute((overloadable)) functionname(              char,               char,               char); \
+    signed      char __attribute((overloadable)) functionname(  signed      char,   signed      char,   signed      char); \
+  unsigned      char __attribute((overloadable)) functionname(unsigned      char, unsigned      char, unsigned      char); \
+
+    __PENCIL_IMPL_DECLARE_OVERLOADS(  min, FLT_FLT)
+    __PENCIL_IMPL_DECLARE_OVERLOADS(  min, INT_INT)
+    __PENCIL_IMPL_DECLARE_OVERLOADS(  max, FLT_FLT)
+    __PENCIL_IMPL_DECLARE_OVERLOADS(  max, INT_INT)
+    __PENCIL_IMPL_DECLARE_OVERLOADS(clamp, FLT_FLT_FLT)
+    __PENCIL_IMPL_DECLARE_OVERLOADS(clamp, INT_INT_INT)
+
+    __PENCIL_IMPL_DECLARE_OVERLOADS( tan , FLT)
+    __PENCIL_IMPL_DECLARE_OVERLOADS(atan , FLT)
+    __PENCIL_IMPL_DECLARE_OVERLOADS(atan2, FLT_FLT)
+    __PENCIL_IMPL_DECLARE_OVERLOADS(hypot, FLT_FLT)
+
+    __PENCIL_IMPL_DECLARE_OVERLOADS(exp, FLT)
+
+    __PENCIL_IMPL_DECLARE_OVERLOADS( ceil, FLT)
+    __PENCIL_IMPL_DECLARE_OVERLOADS(floor, FLT)
+    __PENCIL_IMPL_DECLARE_OVERLOADS(fract, FLT_FLTPTR)
+
+    /* TODO: add more definitions */
+
+#undef __PENCIL_IMPL_DECLARE_OVERLOADS_FLT
+#undef __PENCIL_IMPL_DECLARE_OVERLOADS_FLT_FLT
+#undef __PENCIL_IMPL_DECLARE_OVERLOADS_FLT_FLTPTR
+#undef __PENCIL_IMPL_DECLARE_OVERLOADS_INT_INT
+#undef __PENCIL_IMPL_DECLARE_OVERLOADS_FLT_FLT_FLT
+#undef __PENCIL_IMPL_DECLARE_OVERLOADS_INT_INT_INT
 
 #endif
